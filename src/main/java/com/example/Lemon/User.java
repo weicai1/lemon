079648cee798ca8427b10dfdd6ca24e9269e61b1
weicai1 <wei.cai@stonybrook.edu>
2019-04-07 17:36:51 -0400

    first commit

---
+++ src/main/java/com/example/Lemon/User.java
@@ -0,0 +1,22 @@
+package com.example.Lemon;
+
+
+import org.springframework.context.annotation.Bean;
+
+
+public class User {
+    private int id;
+    private String name;
+    private String password;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/main/resources/templates/13213.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 16312
+  Date: 4/6/2019
+  Time: 4:52 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+
+  $END$
+
+  </body>
+</html>
